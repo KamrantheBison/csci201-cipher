7e1f0cc7c4f0d8daa082a0ea494127b19d504344
Kamran McIntosh-Ross <kamran.mcintoshross@bison.howard.edu>
2026-04-28 01:26:58 +0000

    Apply Caesar cipher shift to alphabetic characters by converting to 0-25 index and mapping back to uppercase letters

--- mycipher.cpp
+++ mycipher.cpp
@@ -4,11 +4,22 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	// get shift from command line
 	int shift = atoi(argv[1]);
     shift = shift % 26;
     string line;
     string encoded = "";
+    // read from stdin
     while (getline(cin, line)) {
         for (char ch : line) {
 
             ch = toupper(ch);
+
+	    // only keep letters
+	     if (ch >= 'A' && ch <= 'Z') {
+                int pos = ch - 'A';
+                int newPos = (pos + shift) % 26;
+                encoded += char(newPos + 'A');
+            }
+        }
+    }
