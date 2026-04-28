0c08cbf555f91845aab8a786f1520d32f4352ff1
Kamran McIntosh-Ross <kamran.mcintoshross@bison.howard.edu>
2026-04-28 01:11:08 +0000

    Iterate through each character of input and normalize to uppercase for consistent cipher processing

--- mycipher.cpp
+++ mycipher.cpp
@@ -6,3 +6,9 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	int shift = atoi(argv[1]);
     shift = shift % 26;
+    string line;
+    string encoded = "";
+    while (getline(cin, line)) {
+        for (char ch : line) {
+
+            ch = toupper(ch);
